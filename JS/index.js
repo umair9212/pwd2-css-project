608acab9f30619cc32e96d35e1ffd254ad56d191
Umair <137381320+umair9212@users.noreply.github.com>
2025-01-09 15:43:34 +0500

    js first

---
+++ JS/index.js
@@ -0,0 +1,19 @@
+document.write('hello world')
+// var let const,
+
+var  a = 10;
+var b =20;
+let c = 30;
+let d = 40;
+const e = 50;
+const f = 60;
+
+var a = 30; 
+c = 40;
+// const e = 70;
+console.log(a+b);
+console.log(c+d);
+console.log(e+f);
+console.warn('sadfsad')
+console.error('sadfadsf')
+console.table('asdf')
